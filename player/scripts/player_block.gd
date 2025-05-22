febe76bd9ed8ac2f7eb6c4a58f62f73dd9cbb97b
Rafael Souza dos Santos <64049095+Radoido@users.noreply.github.com>
2025-05-22 20:18:11 -0300

    adding player

---
+++ player/scripts/player_block.gd
@@ -0,0 +1,22 @@
+class_name player extends CharacterBody2D
+
+var acceleration : float = 200.0
+
+
+
+func _ready() -> void:
+	pass
+	
+	# Called frame per frame
+func _process(delta: float) -> void: 
+	var direction : Vector2 = Vector2.ZERO
+	
+	direction.x = Input.get_action_strength("movePlayerBlockRight") - Input.get_action_strength("movePlayerBlockLeft")
+	 
+	velocity = direction * acceleration
+	
+	pass
+
+	# Has different clock tik from the frames
+func _physics_process(delta: float) -> void:
+	move_and_slide()
